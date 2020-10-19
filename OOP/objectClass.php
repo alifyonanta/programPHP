06a9877754438794f9a7144fc7cafb1a8c31c013
Alif Yonanta <28886550+alifyonanta@users.noreply.github.com>
2020-10-19 19:50:20 +0700

    Update objectClass.php

--- OOP/objectClass.php
+++ OOP/objectClass.php
@@ -29,4 +29,20 @@ $p1 = new Person();
 $p1->age = 23;
 echo $p1->age; 
 $p1->speak();
+
+class Dog {
+    public $legs=4;
+    public function display() {
+        echo $this->legs;
+    }
+}
+$d1 = new Dog();
+$d1->display();
+
+echo '<br />';
+
+$d2 = new Dog();
+$d2->legs = 2;
+$d2->display();
+?>
 ?>
